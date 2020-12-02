2d09f09c17321079c9b0cf3845856ca9f712ecf2
Mika <mikanur10@gmail.com>
2020-12-02 14:09:52 -0500

    MN_madePlayerEdit'

---
+++ client/src/screens/PlayerDetail.jsx
@@ -0,0 +1,47 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { addSkill } from '../services/skills';
+import { getOnePlayer } from '../services/players';
+
+export default function PlayerDetail(props) {
+  const [player, setPlayer] = useState(null);
+  const [skillId, setSkillId] = useState('')
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchPlayer = async () => {
+      const playerData = await getOnePlayer(id);
+      setPlayer(playerData);
+    }
+    fetchPlayer();
+  }, [id])
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const player = await addSkill(skillId, id);
+    setPlayer(player);
+  }
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setSkillId(value);
+  }
+
+  return (
+    <div>
+      <h3>{player?.name}</h3>
+      {player?.skills.map(skill => (
+        <p key={skill.id}>{skill.name}</p>
+      ))}
+      <form onSubmit={handleSubmit}>
+        <select defaultValue='default' onChange={handleChange}>
+          <option disabled value='default'>-- Select a skill --</option>
+          {props.skills.map(skill => (
+            <option value={skill.id} key={skill.id}>{skill.name}</option>
+          ))}
+        </select>
+        <button>add</button>
+      </form>
+    </div>
+  )
+}
