d2ebff67d17be4e9d9bedf53bbbc73e51285cbc1
Mika <mikanur10@gmail.com>
2020-12-05 17:18:43 -0500

    MN_align player card

--- client/src/screens/Players.jsx
+++ client/src/screens/Players.jsx
@@ -4,15 +4,17 @@ import './Players.css'
 
 export default function Players(props) {
   return (
-    <div>
+    <div className="players-container">
       <Link to='/players/new'><button>Create</button></Link>
       <h3>Players</h3>
       {
         props.players.map(player => (
-          <React.Fragment key={player.id}>
-            <div className="player-cards">
+          <div className="player-cards">
+            <React.Fragment key={player.id}>
               <Link to={`/players/${player.id}`}><p>{player.name}</p></Link>
-              <img src={player.img} width="400px" height="300px" alt="player"></img>
+              <div>
+                <img src={player.img} width="400px" height="300px" alt="player"></img>
+              </div>
               <p>{player.position}</p>
               {
                 player.user_id === props.currentUser?.id &&
@@ -21,8 +23,8 @@ export default function Players(props) {
                   <button onClick={() => props.handleDelete(player.id)}>Delete</button>
                 </>
               }
-            </div>
-          </React.Fragment>
+            </React.Fragment>
+          </div>
         ))
       }
       {/* <br /> */}
