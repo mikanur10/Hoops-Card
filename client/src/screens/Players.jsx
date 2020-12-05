9b9752643ba1fbf26578b70b207f80ad9d207e1f
Mika <mikanur10@gmail.com>
2020-12-05 18:01:05 -0500

    MN_addedstyling to elements in player cards

--- client/src/screens/Players.jsx
+++ client/src/screens/Players.jsx
@@ -5,13 +5,13 @@ import './Players.css'
 export default function Players(props) {
   return (
     <div className="players-container">
-      <Link to='/players/new'><button>Create</button></Link>
-      <h3>Players</h3>
+      <Link to='/players/new'><button className="create-button">Create</button></Link>
+      <h3 className="players-title">Players</h3>
       {
         props.players.map(player => (
           <div className="player-cards">
             <React.Fragment key={player.id}>
-              <Link to={`/players/${player.id}`}><p className="player-name">{player.name}</p></Link>
+              <Link className="player-name" to={`/players/${player.id}`}><p>{player.name}</p></Link>
               <div>
                 <img src={player.img} width="400px" height="300px" alt="player"></img>
               </div>
@@ -19,8 +19,8 @@ export default function Players(props) {
               {
                 player.user_id === props.currentUser?.id &&
                 <>
-                  <Link to={`/players/${player.id}/edit`}><button>Edit</button></Link>
-                  <button onClick={() => props.handleDelete(player.id)}>Delete</button>
+                  <Link to={`/players/${player.id}/edit`}><button className="edit-button">Edit</button></Link>
+                  <button className="delete-button" onClick={() => props.handleDelete(player.id)}>Delete</button>
                 </>
               }
             </React.Fragment>
